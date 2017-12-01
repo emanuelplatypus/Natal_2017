4c2f524e14a114d4f4ea414c0e3a039c8f46bc79
emanuelplatypus <manoel.santos@sga.pucminas.br>
2017-12-01 12:06:56 -0200

    Update jabascript.js

--- jabascript.js
+++ jabascript.js
@@ -24,7 +24,7 @@ var close = document.getElementById('close');
 
 function drawList()
 {
-	people.innerHTML = '<option value="">Quem é você?</option>';
+	people.innerHTML = '<option value="">Quem e voce?</option>';
 	for (var i = give.length - 1; i >= 0; i--) {
 		var option = document.createElement('option');
 		option.value = i;
@@ -52,8 +52,8 @@ function selectPerson(person)
 	{
 		receive.push(name);
 	}
-	result.innerHTML = "<h2>" + name + ", você tirou " + recipientName + "!</h2>";
-	close.innerHTML = "<br><br><br><br><br><br>Okay. Clique aqui para apagar o nome da pessoa que você tirou.";
+	result.innerHTML = "<h2>" + name + ", vocÃª tirou " + recipientName + "!</h2>";
+	close.innerHTML = "<br><br><br><br><br><br>Okay. Clique aqui para apagar o nome da pessoa que vocÃª tirou.";
 	if(give.length > 0)
 	{
 		drawList();
